73c8341ffa24883e682ca7955d1f7d154a5f7c03
dylankuka <dylankiyama@alumnos.itr3.edu.ar>
2026-04-28 09:47:38 -0300

    andar

--- iniciar_vision.bat
+++ iniciar_vision.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "C:\xampp\htdocs\vision\vision\vision_app"
-C:\Users\dylan.kiyama.IR3\AppData\Local\Programs\Python\Python311\python.exe app.py
-pause
+start /B C:\Users\dylan.kiyama.IR3\AppData\Local\Programs\Python\Python311\python.exe app.py
+timeout /t 3 /nobreak >nul
+start msedge http://127.0.0.1:5050
